5aca1a849bcc205c15b2325934df6f2c8ca81b1c
Basv1996 <bas.vugts@gmail.com>
2020-06-11 12:06:09 +0200

    some debugging for smaller screen sizes, account page and notification page

--- js/formulier.js
+++ js/formulier.js
@@ -3,12 +3,13 @@ var usernameElement = document.querySelector("#username");
 var passwordElement = document.querySelector("#password");
 var dobElement = document.querySelector("#DOB");
 var countryElement = document.querySelector("#country");
+var emailElement = document.querySelector("#email");
 var volgendSchermKnop = document.querySelector(".goToNextPage");
 
 
 //deze functie checkt elke 100 micro seconden (1/10 seconden) of 1 van de input elementen van het formulier leeg is.
 setInterval(function(){
-    if(usernameElement.value.length == "" || passwordElement.value.length == "" || dobElement.value.length == "" || countryElement.value.length == "") {
+    if(usernameElement.value.length == "" || passwordElement.value.length == "" || dobElement.value.length == "" || countryElement.value.length == "" || emailElement.value.length == "") {
         volgendSchermKnop.hidden=true;
     } else {
     volgendSchermKnop.hidden=false;}
